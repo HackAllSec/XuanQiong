7faa18c5ee8a921a36d0379a5d1b3dd058fba243
NHPT <42366434+NHPT@users.noreply.github.com>
2024-09-10 11:08:36 +0800

    Update users.go

--- backend/models/users.go
+++ backend/models/users.go
@@ -1,98 +1,98 @@
 package models
 
 import (
-	"fmt"
-	"strings"
-	"time"
+    "fmt"
+    "strings"
+    "time"
 
-	"xuanqiong/types"
-	"xuanqiong/utils"
+    "xuanqiong/types"
+    "xuanqiong/utils"
 )
 
 var (
-	user types.User
+    user types.User
 )
 
 // 检查IP是否被锁定
 func IsLocked(ip string) bool {
-	var lockip types.Lockip
-	if err := db.Raw("SELECT * FROM lockip WHERE client_ip = ?", ip).Scan(&lockip).Error; err == nil {
-		if lockip.LockoutUntil != nil && time.Now().Before(*lockip.LockoutUntil) {
-			return true
-		}
-	}
-	return false
+    var lockip types.Lockip
+    if res := db.Raw("SELECT * FROM lockips WHERE client_ip = ?", ip).Scan(&lockip).RowsAffected; res == 1 {
+        if lockip.LockoutUntil != nil && time.Now().Before(*lockip.LockoutUntil) {
+            return true
+        }
+    }
+    return false
 }
 
 // 检查登录凭据
 func CheckLogin(username string, password string) *types.User {
-    err := db.Raw("SELECT * FROM users WHERE username = ?", username).Scan(&user).Error
-	if err != nil {
-		return nil
-	}
-	if user.Status != 1 {
-		return nil
-	}
-	if utils.IsHashEqual(user.Password, password) {
-		token, _ := utils.GenJWTToken(user.Username, user.Role)
-		db.Model(&user).Update("token", token)
-		return &user
-	}
-	return nil
+    res := db.Raw("SELECT * FROM users WHERE username = ?", username).Scan(&user).RowsAffected
+    if res == 0 {
+        return nil
+    }
+    if user.Status != 1 {
+        return nil
+    }
+    if utils.IsHashEqual(user.Password, password) {
+        token, _ := utils.GenJWTToken(user.Username, user.Role)
+        db.Model(&user).Update("token", token)
+        return &user
+    }
+    return nil
 }
 
 // 根据用户名获取用户信息
 func GetUserByUsername(username string) *types.User {
-    err := db.Raw("SELECT * FROM users WHERE username = ?", username).Scan(&user).Error
-	if err != nil {
-		return nil
-	}
-	return &user
+    res := db.Raw("SELECT * FROM users WHERE username = ?", username).Scan(&user).RowsAffected
+    if res == 0 {
+        return nil
+    }
+    return &user
 }
 
 // 根据token获取用户信息
 func GetUserByToken(token string) *types.User {
     if token != "" {
         token = strings.TrimPrefix(token, "Bearer ")
-		err := db.Raw("SELECT * FROM users WHERE token = ?", token).Scan(&user).Error
-		if err != nil {
-			return nil
-		}
-		claims, _ := utils.DecryptJWTToken(token)
-		if claims != nil {
-			return &user
-		}
+        res := db.Raw("SELECT * FROM users WHERE token = ?", token).Scan(&user).RowsAffected
+        if res == 0 {
+            return nil
+        }
+        claims, _ := utils.DecryptJWTToken(token)
+        if claims != nil {
+            return &user
+        }
     }
-	return nil
+    return nil
 }
 
 // 锁定IP地址
 func LockIP(ip string, duration int) {
-	var lockip types.Lockip
-	db.Raw("SELECT * FROM lockip WHERE client_ip = ?", ip).Scan(&lockip)
-	lockoutUntil := time.Now().Add(time.Duration(duration) * time.Minute)
-	lockip.ClientIP = ip
-	lockip.LockoutUntil = &lockoutUntil
-	db.Save(&lockip)
+    var lockip types.Lockip
+    db.Raw("SELECT * FROM lockips WHERE client_ip = ?", ip).Scan(&lockip)
+    lockoutUntil := time.Now().Add(time.Duration(duration) * time.Minute)
+    lockip.ClientIP = ip
+    lockip.LockoutUntil = &lockoutUntil
+    db.Save(&lockip)
 }
 
 // 清除token
 func CleanToken(username string) error {
-	err := db.Raw("SELECT * FROM users WHERE username = ?", username).Scan(&user).Error
-	if err != nil {
-		return err
-	}
-	err = db.Model(&user).Update("token", "").Error
-	if err != nil {
-		return err
-	}
-	return nil
+    res := db.Raw("SELECT * FROM users WHERE username = ?", username).Scan(&user).RowsAffected
+    if res == 0 {
+        return nil
+    }
+    err := db.Model(&user).Update("token", "").Error
+    if err != nil {
+        return err
+    }
+    return nil
 }
 
 // 创建用户
 func CreateUser(username string, password string, role int) error {
-	err := db.Raw("SELECT * FROM users WHERE username = ?", username).Scan(&user).Error
-	if err != nil {
+    res := db.Raw("SELECT * FROM users WHERE username = ?", username).Scan(&user).RowsAffected
+    if res == 0 {
         passwdHash := utils.GenPasswordHash(password)
         userData := types.User{
             Username:   username,
@@ -104,14 +104,14 @@ func CreateUser(username string, password string, role int) error {
         db.Create(&userData)
         return nil
     } else {
-		return fmt.Errorf("User %s already exists.", username)
-	}
+        return fmt.Errorf("User %s already exists.", username)
+    }
 }
 
 // 删除用户
 func DeleteUser(username string) error {
-    result := db.Raw("SELECT * FROM users WHERE username = ?", username).Scan(&user).Error
-    if result.Error != nil {
+    res := db.Raw("SELECT * FROM users WHERE username = ?", username).Scan(&user).RowsAffected
+    if res == 0 {
         return fmt.Errorf("User %s not found.", username)
     }
     db.Delete(&user)
@@ -120,8 +120,8 @@ func DeleteUser(username string) error {
 
 // 启用或禁用用户
 func SetUserStatus(username string, status int) error {
-    result := db.Raw("SELECT * FROM users WHERE username = ?", username).Scan(&user).Error
-    if result.Error != nil {
+    res := db.Raw("SELECT * FROM users WHERE username = ?", username).Scan(&user).RowsAffected
+    if res == 0 {
         return fmt.Errorf("User %s not found.", username)
     }
     db.Model(&user).Update("status", status)
