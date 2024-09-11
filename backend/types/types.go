a2d492a23d8b584ba6bd343fe5d8b5d38b001646
NHPT <42366434+NHPT@users.noreply.github.com>
2024-09-11 17:25:43 +0800

    Update types.go

--- backend/types/types.go
+++ backend/types/types.go
@@ -6,6 +6,41 @@ import (
     "github.com/dgrijalva/jwt-go"
 )
 
+// Config 结构体定义
+type Config struct {
+    Database struct {
+        Type       string `yaml:"type"`
+        Connection struct {
+            Host     string `yaml:"host"`
+            Port     int64    `yaml:"port"`
+            User     string `yaml:"user"`
+            Password string `yaml:"password"`
+            Name     string `yaml:"name"`
+            Charset  string `yaml:"charset"`
+            File     string `yaml:"file"`
+        } `yaml:"connection"`
+    } `yaml:"database"`
+    JWT struct {
+        Secret     string `yaml:"secret"`
+        ExpiresIn  int64 `yaml:"expires_in"`
+    } `yaml:"jwt"`
+    Server struct {
+        Mode          string `yaml:"mode"`
+        Host          string `yaml:"host"`
+        Port          int64 `yaml:"port"`
+        ReadTimeout   int64 `yaml:"read_timeout"`
+        WriteTimeout  int64 `yaml:"write_timeout"`
+    } `yaml:"server"`
+    Log struct {
+        Level string `yaml:"level"`
+        File  string `yaml:"file"`
+    } `yaml:"log"`
+    Login struct {
+        MaxAttempts    int64    `yaml:"max_attempts"`
+        LockoutDuration int64 `yaml:"lockout_duration"`
+    } `yaml:"login"`
+}
+
 // User 用户表
 type User struct {
     ID         uint64      `gorm:"primaryKey"`
