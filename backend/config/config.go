b420d552d33ba14f8d5303bfd6845a1442114d9e
NHPT <42366434+NHPT@users.noreply.github.com>
2024-09-11 17:26:07 +0800

    Update config.go

--- backend/config/config.go
+++ backend/config/config.go
@@ -5,47 +5,15 @@ import (
     "log"
 
     "gopkg.in/yaml.v2"
+    "xuanqiong/types"
 )
 
 var (
-    Config ConfigStruct
+    Config types.Config
     Version = "v1.0.0"
 )
 
-// ConfigStruct 结构体定义
-type ConfigStruct struct {
-    Database struct {
-        Type       string `yaml:"type"`
-        Connection struct {
-            Host     string `yaml:"host"`
-            Port     int64    `yaml:"port"`
-            User     string `yaml:"user"`
-            Password string `yaml:"password"`
-            Name     string `yaml:"name"`
-            Charset  string `yaml:"charset"`
-            File     string `yaml:"file"`
-        } `yaml:"connection"`
-    } `yaml:"database"`
-    JWT struct {
-        Secret     string `yaml:"secret"`
-        ExpiresIn  int64 `yaml:"expires_in"`
-    } `yaml:"jwt"`
-    Server struct {
-        Mode          string `yaml:"mode"`
-        Host          string `yaml:"host"`
-        Port          int64 `yaml:"port"`
-        ReadTimeout   int64 `yaml:"read_timeout"`
-        WriteTimeout  int64 `yaml:"write_timeout"`
-    } `yaml:"server"`
-    Log struct {
-        Level string `yaml:"level"`
-        File  string `yaml:"file"`
-    } `yaml:"log"`
-    Login struct {
-        MaxAttempts    int64    `yaml:"max_attempts"`
-        LockoutDuration int64 `yaml:"lockout_duration"`
-    } `yaml:"login"`
-}
+
 
 func init() {
     data, err := os.ReadFile("config.yaml")
