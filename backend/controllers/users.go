d6b808277515447fd6aeaa10c44914a11fe0e36d
NHPT <42366434+NHPT@users.noreply.github.com>
2024-10-14 18:17:32 +0800

    Update users.go

--- backend/controllers/users.go
+++ backend/controllers/users.go
@@ -25,6 +25,7 @@ func Login(c *gin.Context) {
     }
     loginUser := models.CheckLogin(logindata.Username, logindata.Password)
     if loginUser != nil {
+        maxAttempts = cfg.Login.MaxAttempts
         c.JSON(200, gin.H{"code": 1, "msg":"Login Successful", "username":loginUser.Username, "avatar":loginUser.Avatar, "token": loginUser.Token})
     } else {
         maxAttempts--
@@ -194,4 +195,115 @@ func UpdateUser(c *gin.Context) {
         return
     }
     c.JSON(200, gin.H{"code": 0, "msg": "Permission denied"})
+}
+
+// 获取当前用户信息
+func GetUserInfo(c *gin.Context) {
+    token := c.Request.Header.Get("Authorization")
+    currentUser := models.GetUserByToken(token)
+    if currentUser != nil {
+        totalCount, authCount, _ := models.GetUservulns(currentUser.ID)
+        c.JSON(200, gin.H{"code": 1, "data": gin.H{"username": currentUser.Username,
+            "avatar": currentUser.Avatar, "email": currentUser.Email,
+            "phone": currentUser.Phone, "ranking": currentUser.Ranking,
+            "total": totalCount, "authed": authCount},
+        })
+        return
+    }
+    c.JSON(200, gin.H{"code": 0, "msg": "Permission denied"})
+}
+
+// 修改头像
+func UpdateAvatar(c *gin.Context) {
+    token := c.Request.Header.Get("Authorization")
+    currentUser := models.GetUserByToken(token)
+    if currentUser != nil {
+        file, err := c.FormFile("file")
+        if err != nil {
+            c.JSON(400, gin.H{"code": 2, "msg": "Invalid input"})
+            return
+        }
+        res := models.UpdateAvatar(file, currentUser.ID)
+        c.JSON(200, gin.H{"code": 1, "msg": "Update avatar successfully","data": res})
+        return
+    }
+    c.JSON(200, gin.H{"code": 0, "msg": "Permission denied"})
+}
+
+// 修改用户个人信息
+func UpdateUserInfo(c *gin.Context) {
+    token := c.Request.Header.Get("Authorization")
+    currentUser := models.GetUserByToken(token)
+    if currentUser != nil {
+        var data map[string]interface{}
+        if err := c.ShouldBindJSON(&data); err != nil {
+            c.JSON(400, gin.H{"code": 2, "msg": "Invalid input"})
+            return
+        }
+        username, _ := data["username"].(string)
+        email, _ := data["email"].(string)
+        phone, _ := data["phone"].(string)
+        err := models.UpdateUserInfo(currentUser.ID, username, email, phone)
+        if err != nil {
+            c.JSON(200, gin.H{"code": 3, "msg": err.Error()})
+            return
+        }
+        c.JSON(200, gin.H{"code": 1, "msg": "Update user info successfully"})
+        return
+    }
+    c.JSON(200, gin.H{"code": 0, "msg": "Permission denied"})
+}
+
+// 修改用户密码
+func UpdateUserPassword(c *gin.Context) {
+    token := c.Request.Header.Get("Authorization")
+    currentUser := models.GetUserByToken(token)
+    if currentUser != nil {
+        var data map[string]interface{}
+        if err := c.ShouldBindJSON(&data); err != nil {
+            c.JSON(400, gin.H{"code": 2, "msg": "Invalid input"})
+            return
+        }
+        oldpassword, _ := data["oldpassword"].(string)
+        newpassword, _ := data["newpassword"].(string)
+        err := models.UpdateUserPassword(currentUser.ID, oldpassword, newpassword)
+        if err != nil {
+            c.JSON(200, gin.H{"code": 3, "msg": err.Error()})
+            return
+        }
+        c.JSON(200, gin.H{"code": 1, "msg": "Update user password successfully"})
+        return
+    }
+    c.JSON(200, gin.H{"code": 0, "msg": "Permission denied"})
+}
+
+// 注册用户
+func Register(c *gin.Context) {
+    var data map[string]interface{}
+    if err := c.ShouldBindJSON(&data); err != nil {
+        c.JSON(400, gin.H{"code": 2, "msg": "Invalid input"})
+        return
+    }
+    username, _ := data["username"].(string)
+    password, _ := data["password"].(string)
+    email, _ := data["email"].(string)
+    phone, _ := data["phone"].(string)
+    err := models.Register(username, password, email, phone)
+    if err == 2 {
+        c.JSON(200, gin.H{"code": 2, "msg": "User already exists"})
+        return
+    }
+    if err == 3 {
+        c.JSON(200, gin.H{"code": 3, "msg": "Username and password cannot be the empty"})
+        return
+    }
+    if err == 4 {
+        c.JSON(200, gin.H{"code": 4, "msg": "Email format error"})
+        return
+    }
+    if err == 5 {
+        c.JSON(200, gin.H{"code": 5, "msg": "Email already exists"})
+    }
+    // 注册功能禁用返回code 0
+    c.JSON(200, gin.H{"code": 1, "msg": "Register successfully"})
 }
