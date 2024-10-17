e5aabdb0d47750f65c2313286bf01cee70134876
NHPT <42366434+NHPT@users.noreply.github.com>
2024-10-17 11:48:04 +0800

    Update dbo.go

--- backend/models/dbo.go
+++ backend/models/dbo.go
@@ -3,6 +3,7 @@ package models
 import (
     "fmt"
     "log"
+    "time"
 
     "github.com/natefinch/lumberjack"
     "gorm.io/gorm"
@@ -47,7 +48,7 @@ func init() {
     }
     dsn := generateDSN(config.Config)
     var err error
-    var user types.User
+    var user types.XqUser
     switch config.Config.Database.Type {
     case "mysql":
         db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{Logger: gormLogger})
@@ -65,10 +66,54 @@ func init() {
         log.Fatalf("Error opening database: %v", err)
     }
 
-    res := db.Raw("SELECT * FROM users WHERE username = ?", "admin").Scan(&user)
+    res := db.Raw("SELECT * FROM xq_users WHERE username = ?", "admin").Scan(&user)
     if res.Error != nil {
         log.Println("Initializing Database...")
-        db.AutoMigrate(&types.User{}, &types.Vulnerability{}, &types.Lockip{}, &types.Attachment{})
+        db.AutoMigrate(&types.XqSystemConfig{}, &types.XqJwtConfig{}, &types.XqEmailConfig{}, &types.XqNotice{}, &types.XqUser{},
+            &types.XqVulnType{}, &types.XqVulnerability{}, &types.XqLockip{}, &types.XqAttachment{},
+            &types.XqRankingDetail{}, &types.XqScoreRule{})
+        res = db.First(&types.XqSystemConfig{})
+        if res.RowsAffected == 0 {
+            db.Create(&types.XqSystemConfig{UserRegister: false, MaxAttempts: 5, LockoutDuration: 3600, CreateTime: time.Now()})
+        }
+        res = db.First(&types.XqJwtConfig{})
+        if res.RowsAffected == 0 {
+            db.Create(&types.XqJwtConfig{JwtSecret: utils.GenerateRandomJwtSecret(), JwtExpires: 3600, CreateTime: time.Now()})
+        }
+        res = db.First(&types.XqVulnType{})
+        if res.RowsAffected == 0 {
+            db.Create(&types.XqVulnType{Name: "信息泄露", CreateTime: time.Now()})
+            db.Create(&types.XqVulnType{Name: "远程代码执行", CreateTime: time.Now()})
+            db.Create(&types.XqVulnType{Name: "反序列化", CreateTime: time.Now()})
+            db.Create(&types.XqVulnType{Name: "SQL注入", CreateTime: time.Now()})
+            db.Create(&types.XqVulnType{Name: "跨站脚本攻击", CreateTime: time.Now()})
+            db.Create(&types.XqVulnType{Name: "本地文件包含", CreateTime: time.Now()})
+            db.Create(&types.XqVulnType{Name: "远程文件包含", CreateTime: time.Now()})
+            db.Create(&types.XqVulnType{Name: "跨站请求伪造", CreateTime: time.Now()})
+            db.Create(&types.XqVulnType{Name: "服务器端请求伪造", CreateTime: time.Now()})
+            db.Create(&types.XqVulnType{Name: "任意文件上传", CreateTime: time.Now()})
+            db.Create(&types.XqVulnType{Name: "任意文件读取/下载", CreateTime: time.Now()})
+            db.Create(&types.XqVulnType{Name: "拒绝服务", CreateTime: time.Now()})
+            db.Create(&types.XqVulnType{Name: "暴力破解", CreateTime: time.Now()})
+            db.Create(&types.XqVulnType{Name: "弱口令", CreateTime: time.Now()})
+            db.Create(&types.XqVulnType{Name: "XML外部实体注入", CreateTime: time.Now()})
+            db.Create(&types.XqVulnType{Name: "未授权访问", CreateTime: time.Now()})
+            db.Create(&types.XqVulnType{Name: "水平越权", CreateTime: time.Now()})
+            db.Create(&types.XqVulnType{Name: "垂直越权", CreateTime: time.Now()})
+            db.Create(&types.XqVulnType{Name: "其他", CreateTime: time.Now()})
+        }
+        res = db.First(&types.XqScoreRule{})
+        if res.RowsAffected == 0 {
+            db.Create(&types.XqScoreRule{Type: 1, Rule: "Xray、Nuclei、Goby等完整Poc，误报低", Score: 20, Coefficient: 1.0, CreateTime: time.Now()})
+            db.Create(&types.XqScoreRule{Type: 1, Rule: "Xray、Nuclei、Goby等完整Poc，误报较高", Score: 10, Coefficient: 1.0, CreateTime: time.Now()})
+            db.Create(&types.XqScoreRule{Type: 1, Rule: "仅包含Payload或无法工具化的Poc", Score: 5, Coefficient: 1.0, CreateTime: time.Now()})
+            db.Create(&types.XqScoreRule{Type: 2, Rule: "Xray、Nuclei、Goby等完整Exp，误报低", Score: 30, Coefficient: 1.0, CreateTime: time.Now()})
+            db.Create(&types.XqScoreRule{Type: 2, Rule: "Xray、Nuclei、Goby等完整Exp，误报较高", Score: 15, Coefficient: 1.0, CreateTime: time.Now()})
+            db.Create(&types.XqScoreRule{Type: 2, Rule: "仅包含Payload或无法工具化的Exp", Score: 5, Coefficient: 1.0, CreateTime: time.Now()})
+            db.Create(&types.XqScoreRule{Type: 3, Rule: "互联网资产数大于 5000", Score: 30, Coefficient: 1.0, CreateTime: time.Now()})
+            db.Create(&types.XqScoreRule{Type: 3, Rule: "互联网资产数介于 1000 到 5000", Score: 20, Coefficient: 1.0, CreateTime: time.Now()})
+            db.Create(&types.XqScoreRule{Type: 3, Rule: "互联网资产数小于 1000", Score: 10, Coefficient: 1.0, CreateTime: time.Now()})
+        }
         password, err := utils.GenerateRandomPassword(12)
         if err != nil {
             log.Fatalf("Error generating password: %v", err)
